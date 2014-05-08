2edbf68637f48956add6b86f4aa03456a62552d6
Brandon Goode <brandon.goode@gmail.com>
2014-05-08 14:49:08 -0400

    Updates to Koa REST Example

--- insteon-rest-koa/app.js
+++ insteon-rest-koa/app.js
@@ -4,25 +4,32 @@ var route = require('koa-route');
 var koa = require('koa');
 var app = koa();
 
+app.use(function *(next){
+  try {
+    yield next;
+  } catch (err) {
+    console.log('Error:', err.message);
+    this.status = err.status || 500;
+    this.body = err.message;
+  }
+});
+
 app.use(route.get('/light/:id/on', turnOn));
 app.use(route.get('/light/:id/off', turnOff));
 
 
 function *turnOn(id) {
   var status = yield hub.light(id).turnOn();
-  if(!status.response) {
-    this.throw(404);
+  if(status.response) {
+    this.body = 'on';
   }
-  this.status = 200;
 }
 
 function *turnOff(id) {
   var status = yield hub.light(id).turnOff();
-  console.log('%j', status);
-  if(!status.response) {
-    this.throw(404);
+  if(status.response) {
+    this.body='off';
   }
-  this.status = 200;
 }
 
 hub.connect(process.env.HUB_IP, function () {
